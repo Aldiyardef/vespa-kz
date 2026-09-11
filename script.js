cfbd8bae8ca8ce80f4ea9ee73bb3f2ea60d7da6a
Aldiyardef <m.aldiyar@gmail.com>
2026-09-11 15:56:36 +0500

    поиск

--- script.js
+++ script.js
@@ -375,12 +375,41 @@ document.addEventListener("DOMContentLoaded", () => {
       image.onerror = () => {
         image.remove();
       };
+const gallerySection = galleryContainer?.closest("section");
 
-      image.addEventListener("click", () => {
-        openLightbox(image.src, image.alt);
-      });
+if (galleryContainer && gallerySection) {
+  const previousButton = gallerySection.querySelector(
+    ".gallery-button.prev, .gallery-prev"
+  );
 
-      galleryContainer.appendChild(image);
+  const nextButton = gallerySection.querySelector(
+    ".gallery-button.next, .gallery-next"
+  );
+
+  const scrollGallery = direction => {
+    galleryContainer.scrollBy({
+      left: direction * galleryContainer.clientWidth * 0.8,
+      behavior: "smooth"
+    });
+  };
+
+  if (previousButton) {
+    previousButton.addEventListener("click", () => {
+      scrollGallery(-1);
+    });
+  }
+
+  if (nextButton) {
+    nextButton.addEventListener("click", () => {
+      scrollGallery(1);
+    });
+  }
+}
+image.addEventListener("click", () => {
+  openLightbox(image.src, image.alt);
+});
+
+galleryContainer.appendChild(image);
     }
   }
 
